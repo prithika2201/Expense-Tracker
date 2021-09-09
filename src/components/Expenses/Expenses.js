606847b86009a07aa4569d40de5e72e4c6277c02
prithika2201 <prisansel@gmail.com>
2021-09-09 12:51:12 +0530

    Added keys to list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,32 +2,34 @@ import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
-import ExpensesFilter from './ExpensesFilter'
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
   return (
     <div>
-      <Card className="expenses">
-        
-        <ExpensesFilter selected = {filteredYear} onChangeFilter ={filterChangeHandler}/>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
         {props.items.map((expense) => (
-          <ExpenseItem 
-          title ={expense.title} 
-          amount={expense.amount} 
-          date ={expense.date}
-
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
           />
         ))}
-        
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
